47a77420e51165cce48f5a330297c8e4f1e62337
jonathan <jonathan@funke-kaiser.at>
2024-06-14 14:08:51 +0200

    moved change data button

--- app/src/main/java/at/ac/univie/dailykind/ProfileFragment.java
+++ app/src/main/java/at/ac/univie/dailykind/ProfileFragment.java
@@ -1,11 +1,6 @@
 package at.ac.univie.dailykind;
 
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,13 +12,8 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
     private EditText emailEditText;
@@ -33,17 +23,15 @@ public class ProfileFragment extends Fragment {
     private EditText cityEditText;
     private Spinner countryPicker;
     private Button editButton;
+    private Button calenderButton; // Change this to Button
     private boolean isEditable = false;
     private boolean isPasswordVisible = false;
 
     private ImageButton togglePasswordVisibilityButton;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -51,15 +39,6 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
@@ -78,15 +57,6 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    /*
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
-    */
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -100,6 +70,7 @@ public class ProfileFragment extends Fragment {
         cityEditText = view.findViewById(R.id.city);
         countryPicker = view.findViewById(R.id.country_picker);
         editButton = view.findViewById(R.id.edit_button);
+        calenderButton = view.findViewById(R.id.calenderIcon); // Ensure this matches the ID in the XML
         togglePasswordVisibilityButton = view.findViewById(R.id.toggle_password_visibility);
 
         // Set initial data (Replace with real data)
@@ -131,21 +102,14 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        //load calendar
-        //openCalendarFragmentBelowRecyclerView();
-
-        FloatingActionButton buttonCalendar = view.findViewById(R.id.calenderIcon);
-        buttonCalendar.setOnClickListener(v -> {
+        // Set up the calendar button click listener
+        calenderButton.setOnClickListener(v -> {
             Fragment calendarFrag = new CalendarFragment();
-            Bundle args = new Bundle();
-            calendarFrag.setArguments(args);
-
             getParentFragmentManager().beginTransaction()
                     .replace(R.id.frame_mainActivity, calendarFrag)
                     .addToBackStack(null)
                     .commit();
         });
-
 
         return view;
     }
@@ -168,21 +132,18 @@ public class ProfileFragment extends Fragment {
         editButton.setText(isEditable ? "Save" : "Change Data");
     }
 
-    private void togglePasswordVisibility()
-    {
-        if(isPasswordVisible)
-        {
+    private void togglePasswordVisibility() {
+        if (isPasswordVisible) {
             passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
             togglePasswordVisibilityButton.setImageResource(R.drawable.invisible_eye);
-        }
-        else
-        {
+        } else {
             passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
             togglePasswordVisibilityButton.setImageResource(R.drawable.visible_eye);
         }
 
         isPasswordVisible = !isPasswordVisible;
     }
+
     private void saveProfile() {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
@@ -196,21 +157,4 @@ public class ProfileFragment extends Fragment {
         // For demonstration, show a toast message
         Toast.makeText(getActivity(), "Profile saved!", Toast.LENGTH_SHORT).show();
     }
-
-
-    //calendar section
-  /*  private void openCalendarFragment() {
-        CalendarFragment calendarFragment = new CalendarFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_mainActivity, calendarFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
-    private void openCalendarFragmentBelowRecyclerView() {
-        CalendarFragment calendarFragment = new CalendarFragment();
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.replace(R.id.calendar_container, calendarFragment);
-        transaction.commit();
-    }*/
 }
